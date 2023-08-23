a2e1751513d093796b4efb18f51171c25846a00c
Caio Fabrini <caiofabrini@outlook.com>
2023-08-22 22:49:53 -0300

    criei um print informando que estou na branch de register

--- testeGitflow/testeGitflow/ViewController.swift
+++ testeGitflow/testeGitflow/ViewController.swift
@@ -11,7 +11,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-       print("Olá mundo!!!")
+        print("Olá mundo!!!")
+        print("Olá mundo estou alterando na branch register!!!")
     }
 
 
